3f921bd691ae8a39a42418f1f6817593b491ca57
Xavier Juan Serrador <xavier.juan@scmspain.com>
2016-05-13 14:40:08 +0200

    testing

--- get/lambda_function.py
+++ get/lambda_function.py
@@ -1,5 +1,22 @@
 import json
 
 
+class TestingState:
+
+    status_saved = None
+
+    def set_status(self):
+        self.status_saved = True
+
+    def get_status(self):
+        return self.status_saved
+
 def lambda_handler(event, context):
-    return "this is a GET"
+
+    ola = TestingState()
+    if ola.get_status() == None:
+        ola.set_status()
+        return "Status is not saved, force set status"
+
+    if ola.get_status() == True:
+        return "Status saveedd"
